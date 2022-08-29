fc40e59e2310e43a45770632f9bf097f3884a851
PipoLucido <monntedeoca@hotmail.com>
2022-08-29 14:00:05 -0300

    notificaciones custom id post emit

--- index.js
+++ index.js
@@ -18,41 +18,35 @@ const io = socketIO(server )
 
 let clients_conected = []
 
-
 io.on('connection',(socket)=>{
-
-    
+  
     socket.on('storeClientInfo',(datos)=>{
         //store id para mapear notificacion
         console.log('new conection stablish',socket.id);
         console.log(datos)
         clients_conected = { id_socket: socket.id , negocio_id : datos.negocio_sucursal_id }
     });
-
-
-    socket.on('chatMessage',(datos)=>{
-        //envio notificacion
-        
-        //a un cliente especifico
-        io.sockets.connected[clients_conected.id_socket].emit('chatMessage',datos)
-      
-        //io.sockets.emit('chatMessage',datos)
-        //a todos
-
-       // a todos menos a mi
-       //socket.broadcast.to('chatroom').emit('chatMessage',datos);
-
-
-
-    });
+    
 })
 //conf websocket
 
 //routes
-app.post('/testroute',(req,res) => {
+app.post('/notificacion_nueva',(req,res) => {
   
- //   res.sendFile('index.html', {root: __dirname });
-    console.log('>_ visitaNueva a post test!')
-          
+
+    let datos = {
+        username:" username.value" ,
+        message: "message.value" ,
+        negocio_sucursal_id:"6652"
+        }
+
+
+        io.sockets.connected[clients_conected.id_socket].emit('chatMessage',datos)
+
+
+    console.log('>_ notificacion enviada!')
+        
+    res.send(JSON.stringify(">_ notificacion enviada!"));
+
 });
 //routes
