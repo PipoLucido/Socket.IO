de0e31b8475113bf085c355d528dcb1a77aa1c6f
PipoLucido <monntedeoca@hotmail.com>
2022-08-30 13:00:35 -0300

    express configuration

--- public/noti.js
+++ public/noti.js
@@ -7,5 +7,5 @@ let outputs = document.getElementById('output');
 socket.emit('storeClientInfo', { sucursal_id:"6652" });
 
 socket.on('enviarNotificacion',function(datos){
-    outputs.innerHTML += `<p><strong>${datos.id_pedido}:</strong><br>${datos.sucursal_id}</p><br>`;
+    outputs.innerHTML += `<p><strong>id_pedido: ${datos.id_pedido}</strong><br>sucursal_id: ${datos.sucursal_id}</p><br>`;
 });
